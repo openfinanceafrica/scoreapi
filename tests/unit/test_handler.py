7a9c185b33453169577ad35d709c4d83dc6cfe1c
Mũnyĩrĩ Kamau <munyiri@gmail.com>
2023-03-16 16:27:04 +0300

    fix handler unit test

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -65,6 +65,6 @@ def test_lambda_handler(apigw_event):
     ret = app.lambda_handler(apigw_event, "")
     data = json.loads(ret["body"])
 
-    assert ret["statusCode"] == 100
+    assert ret["statusCode"] == 200
     # assert "message" in ret["body"]
     # assert data["message"] == "hello world"
